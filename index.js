f8f6ec38e8c43ce4cba0080c85dd1c076aaf07a9
tarasabella <tarasabella@gmail.com>
2019-01-28 00:22:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function printBadges(employees) {
+for (let i = 0; i < employees.length; i++) {
+    console.log(`Welcome ${employees[i]}! You are employee #${i+1}.`);
+  }
+  return employees;
+}
+
+function tailsNeverFails () {
+  // set tails to 0
+  let tails = 0;
+  // #create condition to create coin flip
+
+  while (Math.random() >= 0.5) {
+
+  // #itereate through loop adding 1 each time
+    tails++;
+  }
+  // #tells user how many tail flips in a row
+  return `You got ${tails} tails in a row!`;
+}

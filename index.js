18607ea2fb465b3d8791b2f73b770e7c127dda04
Bennimaru <bennywhyu@gamil.com>
2019-02-03 02:58:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function printBadges(array){
+  for(let i=0;i<array.length;i++){
+    console.log(`Welcome ${array[i]}! You are employee #${i+1}.`)
+  }
+  return array
+}
+function tailsNeverFails(){
+  let result = 0
+  while (Math.random() >= 0.5){
+    result ++
+  }
+  return `You got ${result} tails in a row!`
+}
